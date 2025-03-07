1daa2a0a2227c0e52b7cab8d679886054eb5038e
AvinashKadam10 <avinashanilkadam10@gmail.com>
2025-03-07 23:44:06 +0530

    Initial commit

---
+++ contact.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $subject = htmlspecialchars($_POST["subject"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $to = "ambikamantole@gmail.com"; // Your email
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    $fullMessage = "Name: $name\n";
+    $fullMessage .= "Email: $email\n";
+    $fullMessage .= "Subject: $subject\n";
+    $fullMessage .= "Message:\n$message\n";
+
+    if (mail($to, $subject, $fullMessage, $headers)) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+} else {
+    echo "Invalid request";
+}
+?>
